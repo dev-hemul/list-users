3d3f887132f864b735e78c67f93c36ff760b20d8
dev-hemul <dev.hemulll@gmail.com>
2024-10-21 18:24:39 +0300

    add new size form

--- client/src/components/DeleteUser.jsx
+++ client/src/components/DeleteUser.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import axios from 'axios';
 import { MdDelete } from "react-icons/md";
 
-const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost';
+
 
 // Оголошуємо userId та onUserDeleted як пропси в компоненті
 const DeleteUser = ({ userId, onUserDeleted }) => {
   console.log('ID користувача для видалення:', userId); // Логуємо ID користувача
 
   const handleDelete = async () => {
-  console.log(`Відправка запиту DELETE на: http://157.230.115.142:4000/deleteUser/${userId}`); // Логуємо URL
+  console.log(`Відправка запиту DELETE на: https://evgeniiviter.site/deleteUser/${userId}`); // Логуємо URL
+    const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:4000';
   try {
     const response = await axios.delete(`${apiUrl}/deleteUser/${userId}`);
     if (response.status === 200) {
--- client/src/components/UpdateUser.jsx
+++ client/src/components/UpdateUser.jsx
@@ -2,54 +2,50 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { BiSolidPencil } from "react-icons/bi";
 
-// Приймаємо userId та onUserUpdated як пропси для використання у компоненті
-const UpdateUser = ({ userId, onUserUpdated }) => {
-  const [isEditing, setIsEditing] = useState(false); // Стан редагування
-  const [newName, setNewName] = useState(''); // Стан нового імені
+const UpdateUser = ({ userId, currentName, onUserUpdated }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newName, setNewName] = useState(currentName); // Инициализируем с текущим именем
 
-  // Обробник введення для зміни імені
   const handleInputChange = (e) => {
-    console.log('Нове ім\'я:', e.target.value); // Логуємо нове ім'я
     setNewName(e.target.value);
   };
 
-  // Обробник збереження змін
   const handleSaveClick = async () => {
-    console.log('Збереження користувача...'); // Перевіряємо чи функція викликається
     try {
-			const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost';
+      const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:4000';
       const response = await axios.put(`${apiUrl}/updateUser/${userId}`, { name: newName });
-      console.log('Відповідь серверу:', response); // Перевіряємо відповідь від сервера
       if (response.status === 200) {
-        console.log('Користувач успішно оновленений', response.data);
-        onUserUpdated(userId, newName); // Оновлюємо дані користувача на фронті
-        setIsEditing(false); // Закриваємо режим редагування
+        onUserUpdated(userId, newName);
+        setIsEditing(false); // Закрываем режим редактирования
       }
     } catch (error) {
-      console.error('Помилка при оновленні користувача:', error);
+      console.error('Error updating user:', error);
     }
   };
 
-  // Відображаємо різні кнопки в залежності від того, чи редагується користувач, чи ні
   return (
-    <div>
+    <div className="flex items-center">
       {isEditing ? (
-        <div>
+        <div className="flex flex-wrap items-center">
           <input
             type="text"
             value={newName}
-            onChange={handleInputChange} // При введенні змінюємо ім'я
-            placeholder="Введіть нове ім'я"
+            onChange={handleInputChange}
+            placeholder="Введите новое имя"
+            className="border border-gray-300 p-1 rounded w-full mb-5"
           />
-          <div className="flex gap-10 mt-5">
-          <button className="border border-green-500 p-2" onClick={handleSaveClick}>Зберегти</button> {/* Кнопка для збереження змін */}
-          <button className="border border-red-500 p-2" onClick={() => setIsEditing(false)}>Скасувати</button> {/* Кнопка для скасування редагування */}
+          <div className="flex">
+          <button className="border border-green-500 p-2" onClick={handleSaveClick}>Сохранить</button>
+          <button className="border border-red-500 p-2 ml-2" onClick={() => setIsEditing(false)}>Отмена</button>
           </div>
         </div>
       ) : (
-        <button onClick={() => setIsEditing(true)}> {/* Кнопка для увімкнення режиму редагування */}
-          <BiSolidPencil className="size-5 mr-3" />
-        </button>
+        <div className="flex items-center">
+          <span className="mr-2">{currentName}</span> {/* Имя пользователя отображается здесь */}
+          <button onClick={() => setIsEditing(true)}>
+            <BiSolidPencil className="size-5 mr-3" />
+          </button>
+        </div>
       )}
     </div>
   );
