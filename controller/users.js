d535d0ee45dc92d0bb4ba8d2c8ebac46a80b2dae
dev-hemul <dev.hemulll@gmail.com>
2024-10-23 12:35:10 +0300

    add logs for controller

--- controller/users.js
+++ controller/users.js
@@ -8,6 +8,7 @@ const createUser = async (name, userIp, userAgent, referer, acceptLanguage) => {
     referrer: referer,
     acceptLanguage: acceptLanguage,
   });
+  console.log(result);
   return result.id;
 };
 
